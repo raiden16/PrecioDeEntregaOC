f46be4b575961fb6d212d17b2efb50e87ae26d98
Luis Luna <siul-luna@hotmail.com>
2021-09-23 16:50:30 -0500

    Finalizado

---
+++ PrecioDeEntregaOC/CatchingEvents.vb
@@ -0,0 +1,169 @@
+﻿Public Class CatchingEvents
+
+    Friend WithEvents SBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Friend SBOCompany As SAPbobsCOM.Company '//OBJETO COMPAÑIA
+    Friend csDirectory As String '//DIRECTORIO DONDE SE ENCUENTRAN LOS .SRF
+
+    Public Sub New()
+
+        MyBase.New()
+        SetAplication()
+        SetConnectionContext()
+        ConnectSBOCompany()
+
+        setFilters()
+
+    End Sub
+
+    '//----- ESTABLECE LA COMUNICACION CON SBO
+    Private Sub SetAplication()
+        Dim SboGuiApi As SAPbouiCOM.SboGuiApi
+        Dim sConnectionString As String
+        Try
+            SboGuiApi = New SAPbouiCOM.SboGuiApi
+            sConnectionString = Environment.GetCommandLineArgs.GetValue(1)
+            SboGuiApi.Connect(sConnectionString)
+            SBOApplication = SboGuiApi.GetApplication()
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con la aplicación SBO " & ex.Message)
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+        End Try
+    End Sub
+
+    '//----- ESTABLECE EL CONTEXTO DE LA APLICACION
+    Private Sub SetConnectionContext()
+        Try
+            SBOCompany = SBOApplication.Company.GetDICompany
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con el DI")
+            SBOApplication = Nothing
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+            'Finally
+        End Try
+    End Sub
+
+    '//----- CONEXION CON LA BASE DE DATOS
+    Private Sub ConnectSBOCompany()
+        Dim loRecSet As SAPbobsCOM.Recordset
+        Try
+            '//ESTABLECE LA CONEXION A LA COMPAÑIA
+            csDirectory = My.Application.Info.DirectoryPath
+            If (csDirectory = "") Then
+                System.Windows.Forms.Application.Exit()
+                End
+            End If
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con la BD. " & ex.Message)
+            SBOApplication = Nothing
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+        Finally
+            loRecSet = Nothing
+        End Try
+    End Sub
+
+    '//----- ESTABLECE FILTROS DE EVENTOS DE LA APLICACION
+    Private Sub setFilters()
+        Dim lofilter As SAPbouiCOM.EventFilter
+        Dim lofilters As SAPbouiCOM.EventFilters
+
+        Try
+            lofilters = New SAPbouiCOM.EventFilters
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_FORM_LOAD)
+            lofilter.AddEx(142) '// FORMA Pedido Proveedores
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED)
+            lofilter.AddEx(142) '// FORMA Pedido Proveedores
+
+            SBOApplication.SetFilter(lofilters)
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("SetFilter: " & ex.Message)
+        End Try
+
+    End Sub
+
+    ''//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ''// METODOS PARA EVENTOS DE LA APLICACION
+    ''//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Private Sub SBO_Application_AppEvent(ByVal EventType As SAPbouiCOM.BoAppEventTypes) Handles SBOApplication.AppEvent
+        Select Case EventType
+            Case SAPbouiCOM.BoAppEventTypes.aet_ShutDown, SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition, SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged
+                System.Windows.Forms.Application.Exit()
+                End
+        End Select
+
+    End Sub
+
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    '// METODOS PARA MANEJO DE EVENTOS ITEM
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub SBOApplication_ItemEvent(ByVal FormUID As String, ByRef pVal As SAPbouiCOM.ItemEvent, ByRef BubbleEvent As Boolean) Handles SBOApplication.ItemEvent
+
+        If pVal.Before_Action = True And pVal.FormTypeEx <> "" Then
+        Else
+            If pVal.Before_Action = False And pVal.FormTypeEx <> "" Then
+                Select Case pVal.FormTypeEx
+
+                    Case 142                           '////// FORMA Pedido proveedores
+                        frmOPORControllerAfter(FormUID, pVal)
+
+                End Select
+            End If
+        End If
+
+    End Sub
+
+
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    '// CONTROLADOR DE EVENTOS FORMA Estados de cuenta externos
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub frmOPORControllerAfter(ByVal FormUID As String, ByVal pVal As SAPbouiCOM.ItemEvent)
+
+        Dim obotton As OPOR
+        Dim oOPOR As FrmtekOPOR
+        Dim stTabla As String
+        Dim coForm As SAPbouiCOM.Form
+        Dim oDatatable As SAPbouiCOM.DBDataSource
+        Dim CardCode As String
+
+        Try
+
+            Select Case pVal.EventType
+
+                '///// Carga de formas
+                Case SAPbouiCOM.BoEventTypes.et_FORM_LOAD
+                    obotton = New OPOR
+                    obotton.addFormItems(FormUID)
+
+                '//////Evento Presionar Item
+                Case SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED
+
+                    Select Case pVal.ItemUID
+
+                        Case "btPriDl"
+
+                            stTabla = "OPOR"
+                            coForm = SBOApplication.Forms.Item(FormUID)
+
+                            oDatatable = coForm.DataSources.DBDataSources.Item(stTabla)
+                            CardCode = oDatatable.GetValue("CardCode", 0)
+
+                            oOPOR = New FrmtekOPOR
+                            oOPOR.openForm(csDirectory, CardCode)
+
+                    End Select
+
+            End Select
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("Error en el evento sobre Forma Facturacion Clientes. " & ex.Message)
+        Finally
+            'oPO = Nothing
+        End Try
+    End Sub
+
+
+End Class
---
+++ PrecioDeEntregaOC/OPOR.vb
@@ -0,0 +1,55 @@
+﻿Public Class OPOR
+
+    Private cSBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Private cSBOCompany As SAPbobsCOM.Company     '//OBJETO DE CONEXION
+    Private coForm As SAPbouiCOM.Form           '//FORMA
+    Private Directorio As String
+
+    '//----- METODO DE CREACION DE LA CLASE
+    Public Sub New()
+        MyBase.New()
+        cSBOApplication = oCatchingEvents.SBOApplication
+        cSBOCompany = oCatchingEvents.SBOCompany
+        Directorio = oCatchingEvents.csDirectory
+    End Sub
+
+    '//----- AGREGA ELEMENTOS A LA FORMA
+    Public Sub addFormItems(ByVal FormUID As String)
+        Dim loItem As SAPbouiCOM.Item
+        Dim loButton As SAPbouiCOM.Button
+        Dim lsItemRef As String
+
+        Try
+
+            '//AGREGA BOTON MOVIMIENTOS EN PEDIDOS DE COMPRAS
+            coForm = cSBOApplication.Forms.Item(FormUID)
+
+            'If coForm.Items.Item("btCost").Enabled = True Then
+            'lsItemRef = "btCost"
+            'Else
+            lsItemRef = "2"
+            'End If
+            '"btPriDl" Para que funcione primero corre este, despues el de ExtraPriceItems y por ultimo el de movimientos
+
+            loItem = coForm.Items.Add("btPriDl", SAPbouiCOM.BoFormItemTypes.it_BUTTON)
+            If cSBOCompany.UserSignature = 25 Or cSBOCompany.UserSignature = 1 Then
+                loItem.Left = coForm.Items.Item(lsItemRef).Left + coForm.Items.Item(lsItemRef).Width + coForm.Items.Item(lsItemRef).Width + coForm.Items.Item(lsItemRef).Width + coForm.Items.Item(lsItemRef).Width + 30
+            ElseIf cSBOCompany.UserSignature = 37 Then
+                loItem.Left = coForm.Items.Item(lsItemRef).Left + coForm.Items.Item(lsItemRef).Width + 10
+            End If
+            loItem.Top = coForm.Items.Item(lsItemRef).Top
+            loItem.Width = coForm.Items.Item(lsItemRef).Width + 40
+            loItem.Height = coForm.Items.Item(lsItemRef).Height
+            loButton = loItem.Specific
+            loButton.Caption = "Precio de Entrega"
+
+        Catch ex As Exception
+            cSBOApplication.MessageBox("DocumentoSBO. agregar elementos a la forma. " & ex.Message)
+        Finally
+            coForm = Nothing
+            loItem = Nothing
+            loButton = Nothing
+        End Try
+    End Sub
+
+End Class
